86873a46e89dc26d2a0d679fd4055bdc1d29fb81
kon <sound.recording.man@gmail.com>
2016-02-17 13:51:04 +0900

    Yuv422の処理をModelに移動

---
+++ YuvViewer/Model/Yuv422.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace YuvViewer.Model
+{
+    public static class Yuv422
+    {
+        public static WriteableBitmap GetImage(string path, int width, int height)
+        {
+            // Y,Cb,Crのbyte列を取得
+            byte[] lum, cb, cr;
+            int ret;
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                // byte列の確保
+                lum = new byte[fs.Length / 2];
+                cb = new byte[lum.Length / 2];
+                cr = new byte[lum.Length / 2];
+
+                // 読込み
+                ret = fs.Read(lum, 0, lum.Length);
+                ret = fs.Read(cr, 0, cr.Length);
+                ret = fs.Read(cb, 0, cb.Length);
+            }
+
+            double temp;
+            WriteableBitmap wbmp = new WriteableBitmap(
+                width, height, 96, 96, PixelFormats.Bgr32, null);
+
+            wbmp.Lock();
+
+            byte[] buf = new byte[wbmp.PixelWidth * wbmp.PixelHeight * 4];
+            for (int i = 0; i < buf.Length; i += 4)
+            {
+                int index = i / 4;
+                // B
+                temp = 1.164 * (lum[index] - 16) + 2.018 * (cr[index / 2] - 128);
+                if (temp > 255) temp = 255;
+                if (temp < 0) temp = 0;
+                buf[i] = (byte)temp;
+
+                // G
+                temp = 1.164 * (lum[index] - 16) - 0.391 * (cr[index / 2] - 128) - 0.813 * (cb[index / 2] - 128);
+                if (temp > 255) temp = 255;
+                if (temp < 0) temp = 0;
+                buf[i + 1] = (byte)temp;
+
+                // R
+                temp = 1.164 * (lum[index] - 16) + 1.596 * (cb[index / 2] - 128);
+                if (temp > 255) temp = 255;
+                if (temp < 0) temp = 0;
+                buf[i + 2] = (byte)temp;
+            }
+
+            Marshal.Copy(buf, 0, wbmp.BackBuffer, buf.Length);
+
+            wbmp.AddDirtyRect(new Int32Rect(0, 0, wbmp.PixelWidth, wbmp.PixelHeight));
+            wbmp.Unlock();
+
+            return wbmp;
+        }
+    }
+}
--- YuvViewer/ViewModel/MainViewModel.cs
+++ YuvViewer/ViewModel/MainViewModel.cs
@@ -9,11 +9,13 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using YuvViewer.Model;
 
 namespace YuvViewer.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Properties
         private uint inputWidth;
         public uint InputWidth
         {
@@ -46,7 +48,8 @@ namespace YuvViewer.ViewModel
                 NotifyPropertyChanged("InputImage");
             }
         }
-
+        #endregion
+        
         public MainViewModel()
         {
             InputWidth = 5376;
@@ -57,56 +60,9 @@ namespace YuvViewer.ViewModel
         {
             Stopwatch sw = new Stopwatch();
 
-            // Y,Cb,Crのbyte列を取得
-            byte[] lum, cb, cr;
-            int ret;
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
-            {
-                // byte列の確保
-                lum = new byte[fs.Length / 2];
-                cb = new byte[lum.Length / 2];
-                cr = new byte[lum.Length / 2];
-
-                // 読込み
-                ret = fs.Read(lum, 0, lum.Length);
-                ret = fs.Read(cr, 0, cr.Length);
-                ret = fs.Read(cb, 0, cb.Length);
-            }
-
-            double temp;
-            WriteableBitmap wbmp = new WriteableBitmap(
-                (int)InputWidth, (int)InputHeight, 96, 96, PixelFormats.Bgr32, null);
-
             sw.Start();
-            wbmp.Lock();
 
-            byte[] buf = new byte[wbmp.PixelWidth * wbmp.PixelHeight * 4];
-            for (int i = 0; i < buf.Length; i += 4)
-            {
-                int index = i / 4;
-                // B
-                temp = 1.164 * (lum[index] - 16) + 2.018 * (cr[index / 2] - 128);
-                if (temp > 255) temp = 255;
-                if (temp < 0) temp = 0;
-                buf[i] = (byte)temp;
-
-                // G
-                temp = 1.164 * (lum[index] - 16) - 0.391 * (cr[index / 2] - 128) - 0.813 * (cb[index / 2] - 128);
-                if (temp > 255) temp = 255;
-                if (temp < 0) temp = 0;
-                buf[i+1] = (byte)temp;
-                
-                // R
-                temp = 1.164 * (lum[index] - 16) + 1.596 * (cb[index / 2] - 128);
-                if (temp > 255) temp = 255;
-                if (temp < 0) temp = 0;
-                buf[i+2] = (byte)temp;
-            }
-
-            Marshal.Copy(buf, 0, wbmp.BackBuffer, buf.Length);
-
-            wbmp.AddDirtyRect(new Int32Rect(0, 0, wbmp.PixelWidth, wbmp.PixelHeight));
-            wbmp.Unlock();
+            WriteableBitmap wbmp = Yuv422.GetImage(path, (int)InputWidth, (int)InputHeight);
 
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds + "ms");
